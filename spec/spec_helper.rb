0bdcacaf7f14220e8ad30e42da9e7d88a61437d1
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-08-29 11:36:43 +0100

    set up simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,15 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'rspec'
 require 'capybara/rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 Capybara.app = BookmarkManager
 
